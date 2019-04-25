5d227a03d6dd4caf07b9521afeedd401f4cdde5d
Rodrigo Fonseca Martins <rodrigo.fonsecamartins@gmail.com>
2019-04-25 00:28:04 -0300

    arrumado path

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const path = require('path')
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname,'views'))
+app.use(express.static(path.join(__dirname,'public')))
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res)=>{
