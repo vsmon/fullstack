03414c2a2e13e16dc29cd0fc88b5f3f66de21ae8
Rodrigo Fonseca Martins <rodrigo.fonsecamartins@gmail.com>
2019-04-24 23:59:22 -0300

    path views corrgidas

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 const express = require('express')
 const app = express()
 
+const path = require('path')
+
 app.set('view engine', 'ejs')
-//Now
+app.set('views', path.join(__dirname,'views'))
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res)=>{
-    res.send('ok')
+    res.send('Home')
 })
 
-app.get('/Home', (req, res)=>{
-    res.render('Home')
-})
 
 app.listen(port, (error)=>{
     if(error){
